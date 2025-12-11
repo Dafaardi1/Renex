64b7d6b67ada313ad743c427d7a462b4f05871ec
Dafaardi1 <dafa.ardi17@gmail.com>
2025-12-11 16:23:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-print("hello world")
+import streamlit as st
+
+st.title("Hitung Luas Persegi Panjang")
+
+panjang = st.number_input ("masukan nilai panjang", 0)
+lebar= st.number_input ("masukkan nilai lebar")
+hitung= st.button("hitung luas")
